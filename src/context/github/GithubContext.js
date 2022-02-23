ae02491661ff5255a8462fcb1bfa6eec05eb8626
Dave <87390532+vadedes@users.noreply.github.com>
2022-02-24 02:52:17 +0800

    added search functions and implmented reducers

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
@@ -6,23 +7,46 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: false,
+  };
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  // Get inital users (testing purposes)
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
 
-    setUsers(data);
-    setLoading(false);
+    dispatch({
+      type: 'GET_USERS',
+      payload: items, //payload is passed on as an array filled with queried data
+    });
   };
 
-  return <GithubContext.Provider value={{ users, loading, fetchUsers }}>{children}</GithubContext.Provider>;
+  //clear users
+  const clearUsers = () => {
+    dispatch({
+      type: 'CLEAR_USERS',
+    });
+  };
+
+  //Set Loading
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
+
+  return <GithubContext.Provider value={{ users: state.users, loading: state.loading, searchUsers, clearUsers }}>{children}</GithubContext.Provider>;
 };
 
 export default GithubContext;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 
 function Home() {
   return (
     <>
-      {/* SEARCH COMPONENT HERE */}
+      <UserSearch />
       <UserResults />
     </>
   );
